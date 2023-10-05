2cee0d2903c592acb293eb007ea5edad3b38e259
Rafael Brendo <rafalive122@hotmail.com>
2023-10-05 18:20:48 -0300

    Criando e testando a estrutura Fila.

---
+++ fila/fila/src/main/java/main.java
@@ -0,0 +1,29 @@
+import model.Fila;
+import model.No;
+
+public class main {
+    public static void main(String[] args){
+
+        Fila minhaFila = new Fila();
+        //FIFO(First in, First out)
+        minhaFila.enqueue(new No("primeiro"));
+        minhaFila.enqueue(new No("segundo"));
+        minhaFila.enqueue(new No("terceiro"));
+        minhaFila.enqueue(new No("quarto"));
+
+        System.out.println(minhaFila);
+        System.out.println();
+        System.out.println(minhaFila.dequeue());
+        System.out.println(minhaFila.dequeue());
+        System.out.println(minhaFila.dequeue());
+
+        System.out.println(minhaFila.first());
+
+        minhaFila.enqueue(new No("ultimo"));
+
+        System.out.println(minhaFila);
+
+        System.out.println(minhaFila.first());
+
+    }
+}
