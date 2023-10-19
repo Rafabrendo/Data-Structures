7fd78a7d9c5cbe221fbd72fe63642c5dad0e6f55
Rafael Brendo <rafalive122@hotmail.com>
2023-10-19 15:06:00 -0300

    Implementando alguns métodos.

---
+++ listaDuplamenteEncadeada/listaDuplamenteEncadeadaEx/src/main/java/model/ListaDuplamenteEncadeada.java
@@ -0,0 +1,21 @@
+package model;
+
+public class ListaDuplamenteEncadeada<T> {
+
+    private NoDuplo<T> primeiroNo;
+    private NoDuplo<T> ultimoNo;
+
+    private int tamanhoLista;
+    //Adicionei esse metodo para tirar a necessidade de percorrer a lista para saber o tamanho dela.
+
+    public ListaDuplamenteEncadeada(){
+        this.primeiroNo = null;
+        this.ultimoNo = null;
+        this.tamanhoLista = 0;
+    }
+
+    public int size(){
+        return tamanhoLista;
+    }
+
+}
