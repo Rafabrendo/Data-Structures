d85885cf0bd2c148dcb61672ae26141c90562ec1
Rafael Brendo <rafalive122@hotmail.com>
2023-10-19 15:33:08 -0300

    Implementando método add.

--- listaDuplamenteEncadeada/listaDuplamenteEncadeadaEx/src/main/java/model/ListaDuplamenteEncadeada.java
+++ listaDuplamenteEncadeada/listaDuplamenteEncadeadaEx/src/main/java/model/ListaDuplamenteEncadeada.java
@@ -19,6 +19,20 @@ public class ListaDuplamenteEncadeada<T> {
         //Como eu estou retornando o index e não o no, é só retornar o conteudo dele.
     }
 
+    public void add(T elemento){
+        NoDuplo<T> novoNo = new NoDuplo<>(elemento);
+        novoNo.setNoProximo(null);
+        novoNo.setNoPrevio(ultimoNo);
+        if(primeiroNo == null){
+            primeiroNo = novoNo;
+        }
+        if(ultimoNo != null){
+            ultimoNo.setNoProximo(novoNo);
+        }
+        ultimoNo = novoNo;
+        tamanhoLista++;
+    }
+
     private NoDuplo<T> getNo(int index){
         NoDuplo<T> noAuxiliar = primeiroNo;
         for(int i= 0; (i < index) && (noAuxiliar != null); i++){
@@ -28,7 +42,7 @@ public class ListaDuplamenteEncadeada<T> {
     }
 
     public int size(){
-        return tamanhoLista;
+        return this.tamanhoLista;
     }
 
 }
