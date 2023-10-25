3f8272c8b733f6c5e8675a6df21569d762f9c5ac
Rafael Brendo <rafalive122@hotmail.com>
2023-10-25 17:48:40 -0300

    Criando métodos na lista circular

---
+++ listaCircular/lista_circular/src/main/java/model/ListaCircular.java
@@ -0,0 +1,18 @@
+package model;
+
+public class ListaCircular<T> {
+
+    private No<T> cabeca;
+    private No<T> cauda;
+    private int tamanhoLista;
+
+    public boolean isEmpty(){
+        return this.tamanhoLista == 0 ? true : false;
+    }
+
+    public int size(){
+        return this.tamanhoLista;
+    }
+
+
+}
