efdab77483c7044330662fad49a8b45180e2eb21
Rafael Brendo <rafalive122@hotmail.com>
2023-11-21 16:12:15 -0300

    Implementando a classe ArvoreBinaria

---
+++ arvoreBinaria/arvoresbinarias/src/main/java/model/ArvoreBinaria.java
@@ -0,0 +1,27 @@
+package model;
+
+public class ArvoreBinaria<T extends Comparable<T>>{
+
+    private BinNo<T> raiz;
+
+    public ArvoreBinaria(){
+        this.raiz = null;
+    }
+
+    public void inserir(T conteudo){
+            BinNo<T> novoNo = new BinNo<>(conteudo);
+            raiz = inserir(raiz, novoNo);//Esse metodo vai chamar o de baixo
+    }
+
+    private BinNo<T> inserir(BinNo<T> atual, BinNo<T> novoNo){
+        if(atual == null){
+            return novoNo;
+        } else if(novoNo.getConteudo().compareTo(atual.getConteudo())< 0){
+            atual.setNoEsq(inserir(atual.getNoEsq(), novoNo));
+        }else{
+            atual.setNoDir(inserir(atual.getNoDir(), novoNo));
+        }
+        return atual;
+    }
+
+}
